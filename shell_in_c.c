be04b36dd1147dc001ab591c0488205cecc7724f
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-06-24 12:33:58 +0100

    first commit

---
+++ shell_in_c.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int spawn_child(char *command)
+{
+    pid_t pid;
+    int exec;
+    pid = fork();
+    
+    //Child process has pid of 0
+    if(pid == 0)
+    {
+        //printf("I am the child\n");
+        char *args[] = {command, NULL};
+        exec = execv(args[0], args);
+        if (exec == -1)
+        {
+            printf("Unable to launch %s\n", command);
+            exit(0);
+        }
+    }
+    else if(pid == -1)
+    {
+        printf("Unable to fork process\n");
+    }
+    //Parent process has a non zero pid
+    else 
+    {
+        wait(0);
+        //printf("I am the parent\n");
+    }
+}
+int get_command(char *command, int *res)
+{
+    *res = scanf("%s", command);
+}
+
+int main (void)
+{
+    char command[256];
+    int res;
+    while(1)
+    {
+        printf("$ ");
+        get_command(command, &res);
+        if(res == EOF)
+        {
+            printf("\n");
+            return 0;
+        }
+        else
+            spawn_child(command);
+    }
+    return 0;
+}
---
+++ test_process.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Hello I am the spawned process\n");
+    return 0;
+}
